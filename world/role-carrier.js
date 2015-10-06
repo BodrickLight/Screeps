26d1a455d853f961ae53f1951871769cf210ab59
Dom Light <bodricklight@gmail.com>
2015-10-06 12:23:57 +0100

    Stop carriers taking energy back to miners

--- world/role-carrier.js
+++ world/role-carrier.js
@@ -21,6 +21,21 @@ module.exports = require("role-base")({
  * @param {Creep} creep The creep that should behave as a carrier.
  */
 function carryAction (creep) {
+	if (creep.carry.energy) {
+		// We've got some energy - return to base to drop it off.
+		var target = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+			"filter": x => x.energyCapacity && x.energy < x.energyCapacity,
+		});
+
+		if (target) {
+			creep.moveTo(target);
+			creep.transferEnergy(target);
+			return;
+		}
+
+		// Try to find some more energy while we're waiting to drop it off.
+	}
+
 	if (!creep.memory.target) {
 		// Find the largest stack of dropped energy.
 		var target = _.chain(creep.room.find(FIND_DROPPED_ENERGY))
@@ -36,28 +51,16 @@ function carryAction (creep) {
 		creep.memory.target = { "id": target.id };
 	}
 
-	if (creep.carry.energy < creep.carryCapacity) {
-		// Can still carry more energy, move to the target to pick it up.
-		var target = Game.getObjectById(creep.memory.target.id);
-		if (!target) {
-			// Our energy no longer exists. Try to find some more.
-			delete creep.memory.target;
-			carryAction(creep);
-			return;
-		}
-
-		creep.moveToRange(target, 1);
-		var energy = target.pos.lookFor("energy")[0];
-		creep.pickup(energy);
-	} else {
-		// Full on energy - return to base to drop it off.
-		var target = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
-			"filter": x => x.energyCapacity && x.energy < x.energyCapacity,
-		});
-
-		if (target) {
-			creep.moveTo(target);
-			creep.transferEnergy(target);
-		}
+	// Move to the target to pick up the energy.
+	var target = Game.getObjectById(creep.memory.target.id);
+	if (!target) {
+		// Our energy no longer exists. Try to find some more.
+		delete creep.memory.target;
+		carryAction(creep);
+		return;
 	}
+
+	creep.moveToRange(target, 1);
+	var energy = target.pos.lookFor("energy")[0];
+	creep.pickup(energy);
 }
