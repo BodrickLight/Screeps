35e56267aec8f7ab4de397926f1e2994ffc90bca
Dom Light <bodricklight@gmail.com>
2015-10-07 13:30:09 +0100

    Remove construction planner

--- world/main.js
+++ world/main.js
@@ -6,19 +6,11 @@
 
 require("creepExt");
 var roleFactory = require("roleFactory");
-var constructionPlanner = require("constructionPlanner");
 
 module.exports.loop = loop;
 
 function loop () {
 	var t1 = new Date().getTime();
-
-	// Handle any new constructions.
-	for (const roomId in Game.rooms) {
-		constructionPlanner.handleConstruction(Game.rooms[roomId]);
-	}
-
-	var t2 = new Date().getTime();
 
 	// See if there's any new creeps that should be spawned.
 	for (var spawn of _.values(Game.spawns)) {
@@ -38,7 +30,7 @@ function loop () {
 		}
 	}
 
-	var t3 = new Date().getTime();
+	var t2 = new Date().getTime();
 
 	// Make all the creeps do their actions.
 	for (var creep of _.values(Game.creeps)) {
@@ -48,10 +40,9 @@ function loop () {
 		}
 	}
 
-	var t4 = new Date().getTime();
+	var t3 = new Date().getTime();
 
-	console.log(`constructionPlanner: ${t2 - t1}ms,
-		roleFactory: ${t3 - t2}ms,
-		roles: ${t4 - t3}ms,
-		total: ${t4 - t1}ms`);
+	console.log(`roleFactory: ${t2 - t1}ms,
+		roles: ${t3 - t2}ms,
+		total: ${t3 - t1}ms`);
 }
